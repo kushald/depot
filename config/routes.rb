c748008206fa51b485dcc8f25b8cf08782f449e2
Kushal Dongre <kushz19@gmail.com>
2012-02-20 04:16:51 +0530

    Created catalog , added cart and line_item controller , "also added " button

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Depot::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   resources :products
 
   # The priority is based upon order of creation:
@@ -51,7 +55,7 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-	root :to => "products#index"
+	root :to => "store#index", :as => 'store'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
