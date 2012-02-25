f078244b407c17db48509f0112759c41f27747f6
kushald <kushz19@gmail.com>
2012-02-25 14:44:58 +0530

    added checkout , added link to delete individual items from cart.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -79,7 +79,8 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to(line_items_url) }
+      format.html { redirect_to(store_url) }
+			format.js
       format.xml  { head :ok }
     end
   end
